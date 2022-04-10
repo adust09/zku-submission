9d9cec99cdc3c15faebe800ae66ce48fa7883881
adust09 <shoukitsuda@icloud.com>
2022-04-10 10:28:49 +0900

    add A assignments

--- B/ballot.sol
+++ B/ballot.sol
@@ -1,11 +1,7 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity >=0.7.0 <0.9.0;
 
-/// @title Voting with delegation.
 contract Ballot {
-    // This declares a new complex type which will
-    // be used for variables later.
-    // It will represent a single voter.
     struct Voter {
         uint256 weight; // weight is accumulated by delegation
         bool voted; // if true, that person already voted
@@ -13,54 +9,28 @@ contract Ballot {
         uint256 vote; // index of the voted proposal
     }
 
-    // This is a type for a single proposal.
-    //投票はy/nではなく重みづけで投票する
     struct Proposal {
         bytes32 name; // short name (up to 32 bytes)
         uint256 voteCount; // number of accumulated votes
     }
 
     address public chairperson;
+    uint256 public deadline = block.timestamp + 300 seconds;
 
-    // This declares a state variable that
-    // stores a `Voter` struct for each possible address.s
     mapping(address => Voter) public voters;
 
-    // A dynamically-sized array of `Proposal` structs.
     Proposal[] public proposals;
 
-    /// Create a new ballot to choose one of `proposalNames`.
     constructor(bytes32[] memory proposalNames) {
         chairperson = msg.sender;
         voters[chairperson].weight = 1;
 
-        // For each of the provided proposal names,
-        // create a new proposal object and add it
-        // to the end of the array.
         for (uint256 i = 0; i < proposalNames.length; i++) {
-            // `Proposal({...})` creates a temporary
-            // Proposal object and `proposals.push(...)`
-            // appends it to the end of `proposals`.
             proposals.push(Proposal({name: proposalNames[i], voteCount: 0}));
         }
     }
 
-    uint256 public deadline = block.timestamp + 300 seconds;
-
-    // Give `voter` the right to vote on this ballot.
-    // May only be called by `chairperson`.
-    //投票する権利を渡す
     function giveRightToVote(address voter) external {
-        // If the first argument of `require` evaluates
-        // to `false`, execution terminates and all
-        // changes to the state and to Ether balances
-        // are reverted.
-        // This used to consume all gas in old EVM versions, but
-        // not anymore.
-        // It is often a good idea to use `require` to check if
-        // functions are called correctly.
-        // As a second argument, you can also provide an
-        // explanation about what went wrong.
         require(
             msg.sender == chairperson,
             "Only chairperson can give right to vote."
@@ -70,42 +40,23 @@ contract Ballot {
         voters[voter].weight = 1;
     }
 
-    /// Delegate your vote to the voter `to`.
-    // delegate=代表
     function delegate(address to) external {
-        // assigns reference
         Voter storage sender = voters[msg.sender];
         require(!sender.voted, "You already voted.");
 
         require(to != msg.sender, "Self-delegation is disallowed.");
 
-        // Forward the delegation as long as
-        // `to` also delegated.
-        // In general, such loops are very dangerous,
-        // because if they run too long, they might
-        // need more gas than is available in a block.
-        // In this case, the delegation will not be executed,
-        // but in other situations, such loops might
-        // cause a contract to get "stuck" completely.
         while (voters[to].delegate != address(0)) {
             to = voters[to].delegate;
-
-            // We found a loop in the delegation, not allowed.
             require(to != msg.sender, "Found loop in delegation.");
         }
 
-        // Since `sender` is a reference, this
-        // modifies `voters[msg.sender].voted`
         sender.voted = true;
         sender.delegate = to;
         Voter storage delegate_ = voters[to];
         if (delegate_.voted) {
-            // If the delegate already voted,
-            // directly add to the number of votes
             proposals[delegate_.vote].voteCount += sender.weight;
         } else {
-            // If the delegate did not vote yet,
-            // add to her weight.
             delegate_.weight += sender.weight;
         }
     }
@@ -120,29 +71,16 @@ contract Ballot {
         _;
     }
 
-    /// Give your vote (including votes delegated to you)
-    /// to proposal `proposals[proposal].name`.
-    //投票する
     function vote(uint256 proposal) external voteEnded {
         Voter storage sender = voters[msg.sender];
-        //投票権を有しているかどうかを検証する
         require(sender.weight != 0, "Has no right to vote");
-        //既に投票しているかどうか検証し、二重投票を防ぐ
         require(!sender.voted, "Already voted.");
 
         sender.voted = true;
         sender.vote = proposal;
-
-        // If `proposal` is out of the range of the array,
-        // this will throw automatically and revert all
-        // changes.
-        //投票数を増やす
         proposals[proposal].voteCount += sender.weight;
     }
 
-    /// @dev Computes the winning proposal taking all
-    /// previous votes into account.
-    //勝者を決定する
     function winningProposal() public view returns (uint256 winningProposal_) {
         uint256 winningVoteCount = 0;
         for (uint256 p = 0; p < proposals.length; p++) {
@@ -154,16 +92,7 @@ contract Ballot {
         }
     }
 
-    // Calls winningProposal() function to get the index
-    // of the winner contained in the proposals array and then
-    // returns the name of the winner
-    //勝者をproposals配列から取得して返す
     function winnerName() external view returns (bytes32 winnerName_) {
         winnerName_ = proposals[winningProposal()].name;
     }
-
-    // 各Ballot契約の投票時間を5分
-    // 状態変数 startTime を追加して、投票開始時刻を記録する。
-    // 投票期間が終了したかどうかをチェックする修飾子 voteEnded を作成する。
-    // その修飾子をvote関数で使用し、投票を禁止し、期限後にトランザクションを元に戻す。
 }
